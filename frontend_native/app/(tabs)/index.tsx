5c09c3676a253be21b712b4521e89ec1385505a9
Björn Tenje Persson <bjorn@tenje.se>
2024-11-13 12:41:53 +0100

    removed default pages

--- frontend_native/app/(tabs)/index.tsx
+++ frontend_native/app/(tabs)/index.tsx
@@ -1,14 +1,15 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
 
-export default function TabOneScreen() {
+import { Text, View } from '@/components/Themed';
+import LoginScreen from '@/components/LoginScreen';
+
+export default function() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Login page</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <LoginScreen />
     </View>
   );
 }
