2df4933d297b481ade270d4d89a0f929375aa55e
Björn Tenje Persson <bjorn@tenje.se>
2024-11-14 22:20:43 +0100

    init of react with tailwind and TS

---
+++ react_frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/header'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="min-h-[93vh] flex flex-col justify-center items-center bg-gray-100">
+        <p>hej från kroppen</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ react_frontend/src/components/header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <header className="flex justify-around items-center border-b border-b-gray-400 min-h-[7vh]">
+        <h1>LOGO HERE</h1>
+        <button className="bg-blue-300 w-24 h-12">Login</button>
+      </header>
+    </>
+  )
+}
+
+export default Header;
