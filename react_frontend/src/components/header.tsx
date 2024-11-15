7e5a54521c2cb8dfd175527e7b695776ab354299
Björn Tenje Persson <bjorn@tenje.se>
2024-11-15 09:25:54 +0100

    <a> to <Link>

--- react_frontend/src/components/header.tsx
+++ react_frontend/src/components/header.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const Header = () => {
   return (
     <>
       <header className="flex justify-around items-center border-b border-b-gray-400 min-h-[7vh]">
-        <h1>LOGO HERE</h1>
-        <button className="bg-blue-300 w-24 h-12">Login</button>
+        <Link to="/">LOGO HERE</Link>
+        <Link to="/login">
+          <button className="bg-blue-300 w-24 h-12">Login</button>
+        </Link>
       </header>
       <Outlet />
     </>
