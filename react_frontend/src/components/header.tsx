a13e804c62d45fef5fa5337dcf510df04d53163d
Björn Tenje Persson <bjorn@tenje.se>
2024-11-15 08:05:02 +0100

    Added outlet to header

--- react_frontend/src/components/header.tsx
+++ react_frontend/src/components/header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -7,6 +8,7 @@ const Header = () => {
         <h1>LOGO HERE</h1>
         <button className="bg-blue-300 w-24 h-12">Login</button>
       </header>
+      <Outlet />
     </>
   )
 }
