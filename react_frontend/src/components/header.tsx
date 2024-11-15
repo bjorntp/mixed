9adc35aec1e687addcbf7feca58e348ee6efee27
Björn Tenje Persson <bjorn@tenje.se>
2024-11-15 11:12:07 +0100

    button styling

--- react_frontend/src/components/header.tsx
+++ react_frontend/src/components/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
       <header className="flex justify-around items-center border-b border-b-gray-400 min-h-[7vh]">
         <Link to="/">LOGO HERE</Link>
         <Link to="/login">
-          <button className="bg-blue-300 w-24 h-12">Login</button>
+          <button className="bg-blue-300 px-12 py-3 rounded-md">Login</button>
         </Link>
       </header>
       <Outlet />
--- react_frontend/src/components/newPost.tsx
+++ react_frontend/src/components/newPost.tsx
@@ -12,7 +12,7 @@ const NewPost = () => {
 
   return (
     <div className="h-svh w-svh flex flex-row bg-gray-100 align-middle justify-center ">
-      <form onSubmit={handleSubmit} className="bg-white p-9 my-9 w-fit h-fit border border-black rounded-md">
+      <form onSubmit={handleSubmit} className="bg-white p-9 my-9 w-1/2 h-fit border border-black rounded-md">
         <label className="w-full flex flex-row justify-around py-2">
           <h1>Title: </h1>
           <input type="text" placeholder="Enter the title" onChange={e => setTitle(e.target.value)} value={title} className="w-1/2" />
@@ -22,7 +22,7 @@ const NewPost = () => {
           <input type="text" placeholder="Enter the body" onChange={e => setBody(e.target.value)} value={body} className="w-1/2" />
         </label>
         <div className="w-full flex flex-row justify-end">
-          <button className="p-2 my-2 bg-blue-200 rounded-md" type="submit">Submit post!</button>
+          <button className="p-2 mt-2 bg-blue-200 rounded-md" type="submit">Submit post!</button>
         </div>
       </form>
     </div >
