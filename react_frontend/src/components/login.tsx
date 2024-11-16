e0aac23cbd86c0daa043a4caeb762d5c08b1a637
Björn Tenje Persson <bjorn@tenje.se>
2024-11-16 07:57:16 +0100

    Design changes of login and signup

--- react_frontend/src/components/login.tsx
+++ react_frontend/src/components/login.tsx
@@ -10,19 +10,21 @@ const LoginComponent = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
+    <div className="h-svh w-svh flex flex-row bg-gray-100 align-middle justify-center">
+      <form onSubmit={handleSubmit} className="bg-white p-9 my-9 w-1/2 h-fit border border-black rounded-md" >
+        <label className="w-full flex flex-row justify-around py-2">
           Enter username:
-          <input className="border border-1" name="userName" onChange={e => setUserName(e.target.value)} value={userName} type="text" />
+          <input type="text" placeholder="Enter your user name" name="userName" onChange={e => setUserName(e.target.value)} value={userName} />
         </label>
-        <label>
+        <label className="w-full flex flex-row justify-around py-2">
           Enter password:
           <input className="border border-1" name="password" onChange={e => setPassword(e.target.value)} value={password} type="text" />
         </label>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+        <div className="w-full flex flex-row justify-end">
+          <button className="p-2 mt-2 bg-blue-200 rounded-md" type="submit">Login</button>
+        </div>
+      </form >
+    </div >
   )
 }
 
--- react_frontend/src/components/signup.tsx
+++ react_frontend/src/components/signup.tsx
@@ -21,30 +21,57 @@ const Signup = () => {
   }
 
   return (
-    <div className="h-svh flex flex-col align-middle justify-center">
-      <form className="flex flex-col align-middle justify-around" onSubmit={handleSubmit}>
-        <label className="flex justify-evenly m-12">
-          Enter your email:
-          <input className="border border-1" name="email" onChange={e => setEmail(e.target.value)} value={email} type="text" />
-          {errorEmail && <p>{errorEmail}</p>}
+    <div className="h-svh w-svh flex flex-row bg-gray-100 align-middle justify-center">
+      <form onSubmit={handleSubmit} className="bg-white p-9 my-9 w-1/2 h-fit border border-black rounded-md" >
+        <label className="w-full flex flex-row justify-around py-2">
+          Enter email:
+          <input className="border border-1" type="text" placeholder="Enter your email" name="userName" onChange={e => setUserName(e.target.value)} value={userName} />
         </label>
-        <label className="flex justify-evenly m-12">
-          Enter desired username:
-          <input className="border border-1" name="userName" onChange={e => setUserName(e.target.value)} value={userName} type="text" />
+        <label className="w-full flex flex-row justify-around py-2">
+          Enter username:
+          <input className="border border-1" type="text" placeholder="Enter your desired user name" name="userName" onChange={e => setUserName(e.target.value)} value={userName} />
         </label>
-        <label className="flex justify-evenly m-12">
+        <label className="w-full flex flex-row justify-around py-2">
           Enter password:
-          <input className="border border-1" name="password" onChange={e => setPassword(e.target.value)} value={password} type="password" />
+          <input className="border border-1" name="password" onChange={e => setPassword(e.target.value)} value={password} type="text" />
         </label>
-        <label className="flex justify-evenly m-12">
+        <label className="w-full flex flex-row justify-around py-2">
           Repeat password:
-          <input className="border border-1" name="passwordRepeat" onChange={e => setPasswordRepeat(e.target.value)} value={passwordRepeat} type="password" />
-          <p>{errorPassword}</p>
+          <input className="border border-1" name="password" onChange={e => setPassword(e.target.value)} value={password} type="text" />
         </label>
-        <button type="submit">Sign up!</button>
-      </form>
-    </div>
+        <div className="w-full flex flex-row justify-end">
+          <button className="p-2 mt-2 bg-blue-200 rounded-md" type="submit">Login</button>
+        </div>
+      </form >
+    </div >
+
   )
+
+  //return (
+  //  <div className="h-svh flex flex-col align-middle justify-center">
+  //    <form className="flex flex-col align-middle justify-around" onSubmit={handleSubmit}>
+  //      <label className="flex justify-evenly m-12">
+  //        Enter your email:
+  //        <input className="border border-1" name="email" onChange={e => setEmail(e.target.value)} value={email} type="text" />
+  //        {errorEmail && <p>{errorEmail}</p>}
+  //      </label>
+  //      <label className="flex justify-evenly m-12">
+  //        Enter desired username:
+  //        <input className="border border-1" name="userName" onChange={e => setUserName(e.target.value)} value={userName} type="text" />
+  //      </label>
+  //      <label className="flex justify-evenly m-12">
+  //        Enter password:
+  //        <input className="border border-1" name="password" onChange={e => setPassword(e.target.value)} value={password} type="password" />
+  //      </label>
+  //      <label className="flex justify-evenly m-12">
+  //        Repeat password:
+  //        <input className="border border-1" name="passwordRepeat" onChange={e => setPasswordRepeat(e.target.value)} value={passwordRepeat} type="password" />
+  //        <p>{errorPassword}</p>
+  //      </label>
+  //      <button type="submit">Sign up!</button>
+  //    </form>
+  //  </div>
+  //)
 }
 
 export default Signup;
