59073833193722e931a3bc9ca9b737b892adc5ad
Björn Tenje Persson <bjorn@tenje.se>
2024-11-17 11:15:19 +0100

    JWT and header fixes

--- react_frontend/src/components/header.tsx
+++ react_frontend/src/components/header.tsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { Link, Outlet } from 'react-router-dom';
 
 const Header = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loginSignOut, setLogSign] = useState(<></>);
+
+
+  const api = axios.create(
+    {
+      baseURL: 'http://localhost:3001/api/',
+      withCredentials: true,
+    }
+  );
+  async function checkLoggedIn() {
+
+  }
+
+  useEffect(() => {
+    if (loggedIn) {
+      setLogSign(
+        <button className="bg-blue-300 px-12 py-3 rounded-md">
+          Sign out
+        </button>
+      );
+    } else {
+      setLogSign(
+        <button className="bg-blue-300 px-12 py-3 my-3 rounded-md">
+          Login
+        </button>
+      );
+    }
+  }, [loggedIn]);
+
+
   return (
     <>
       <header className="flex justify-around items-center border-b border-b-gray-400 min-h-[7vh]">
         <Link to="/">LOGO HERE</Link>
         <Link to="/login">
-          <button className="bg-blue-300 px-12 py-3 rounded-md">Login</button>
+          {loginSignOut}
         </Link>
       </header>
       <Outlet />
