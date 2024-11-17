6af91332171bc3cf9d864f8af803e2ca00d98282
Björn Tenje Persson <bjorn@tenje.se>
2024-11-17 10:46:13 +0100

    design of login

--- react_frontend/src/components/login.tsx
+++ react_frontend/src/components/login.tsx
@@ -32,13 +32,13 @@ const LoginComponent = () => {
   return (
     <div className="h-svh w-svh flex flex-row bg-gray-100 align-middle justify-center">
       <form onSubmit={handleSubmit} className="bg-white p-9 my-9 w-1/2 h-fit border border-black rounded-md" >
-        <label className="w-full flex flex-row justify-around py-2">
-          Enter username:
-          <input type="text" placeholder="Enter your user name" name="userName" onChange={e => setUserName(e.target.value)} value={userName} />
+        <label className="w-full grid grid-cols-7 justify-around py-2">
+          <p className="col-start-2" >Enter username:</p>
+          <input className="col-start-5 col-span-2 padding-1 border border-1 rounded-md px-1" type="text" placeholder="Enter your user name" name="userName" onChange={e => setUserName(e.target.value)} value={userName} />
         </label>
-        <label className="w-full flex flex-row justify-around py-2">
-          Enter password:
-          <input className="border border-1" type="password" name="password" onChange={e => setPassword(e.target.value)} value={password} />
+        <label className="w-full grid grid-cols-7 justify-around py-2">
+          <p className="col-start-2">Enter password:</p>
+          <input className="col-start-5 col-span-2 padding-1 border border-1 rounded-md px-1" placeholder="Enter password" type="password" name="password" onChange={e => setPassword(e.target.value)} value={password} />
         </label>
         <div className="w-full flex flex-row justify-end">
           <button className="p-2 mt-2 bg-blue-200 rounded-md" type="submit">Login</button>
