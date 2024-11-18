2f691ed5f0d4ba2fa0abbf205fa13bb3829657b5
Björn Tenje Persson <bjorn@tenje.se>
2024-11-18 13:56:30 +0100

    Errormessages on signup

--- react_frontend/src/components/signup.tsx
+++ react_frontend/src/components/signup.tsx
@@ -7,8 +7,8 @@ const Signup = () => {
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const [passwordRepeat, setPasswordRepeat] = useState('');
-  const [errorPassword, setErrorPassword] = useState('');
-  const [errorEmail, setErrorEmail] = useState('');
+  const [errorPassword, setErrorPassword] = useState(<></>);
+  const [errorEmail, setErrorEmail] = useState(<></>);
   const apiUrl = "http://localhost:3001/api/"
 
   const api = axios.create(
@@ -22,11 +22,15 @@ const Signup = () => {
     e.preventDefault();
     const passwordCheck = password != passwordRepeat;
     if (passwordCheck) {
-      setErrorPassword('The passwords does not match');
+      setErrorPassword(<p>The passwords does not match</p>);
+    } else {
+      setErrorPassword(<></>);
     }
     const emailCheck = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
     if (!emailCheck) {
-      setErrorEmail('Invalid email');
+      setErrorEmail(<p>Invalid email</p>);
+    } else {
+      setErrorEmail(<></>);
     }
     if (!passwordCheck && emailCheck) {
       try {
@@ -50,7 +54,7 @@ const Signup = () => {
     <div className="h-svh w-svh flex flex-row bg-gray-100 align-middle justify-center">
       <form onSubmit={handleSubmit} className="bg-white p-9 my-9 w-1/2 h-fit border border-black rounded-md" >
         <label className="grid grid-cols-7 w-full justify-around py-2">
-          <p className="col-start-2">Enter email:</p>
+          <p className="col-start-2">Enter email:</p>{errorEmail}
           <input className="border border-1 col-start-5 col-span-2" type="text" placeholder="Enter your email" name="userName" onChange={e => setEmail(e.target.value)} value={email} />
         </label>
         <label className="grid grid-cols-7 w-full justify-around py-2">
@@ -62,14 +66,14 @@ const Signup = () => {
           <input className="border border-1 col-start-5 col-span-2" name="password" onChange={e => setPassword(e.target.value)} value={password} type="password" />
         </label>
         <label className="grid grid-cols-7 w-full justify-around py-2">
-          <p className="col-start-2 col-span-2">Repeat password:</p>
+          <p className="col-start-2 col-span-2">Repeat password:</p>{errorPassword}
           <input className="border border-1 col-start-5 col-span-2" name="passwordRepeat" onChange={e => setPasswordRepeat(e.target.value)} value={passwordRepeat} type="password" />
         </label>
         <div className="w-full flex flex-row justify-end">
           <button className="p-2 mt-2 bg-blue-200 rounded-md" type="submit">Login</button>
         </div>
-      </form >
-    </div >
+      </form>
+    </div>
 
   )
 }
