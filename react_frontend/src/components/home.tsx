f281a782a5206992fcafb101ddfb00cf6db16bd6
Björn Tenje Persson <bjorn@tenje.se>
2024-11-19 09:25:00 +0100

    Default card added

--- react_frontend/src/components/home.tsx
+++ react_frontend/src/components/home.tsx
@@ -9,7 +9,7 @@ interface Note {
 }
 
 const HomeComponent = () => {
-  const [notes, setNotes] = useState(<>No posts found!</>);
+  const [notes, setNotes] = useState(<></>);
   const navigate = useNavigate();
 
   const api = axios.create(
@@ -23,13 +23,21 @@ const HomeComponent = () => {
     try {
       const data = await api.get('notes/view').then(res => res.data);
 
-      const cards = data.map((obj: Note, index: number) => (
-        <div key={index} className="bg-white p-9 my-9 w-1/2 h-fit border border-black rounded-md">
-          <h3>Titel: {obj.title}</h3>
-          <p>Inehåll: {obj.body}</p>
-        </div>
-      ));
-      setNotes(cards);
+
+      if (data.length === 0) {
+        setNotes(
+          <div className="bg-white p-9 my-9 w-1/2 h-fit border border-black rounded-md" >
+            <p>No posts found!</p>
+          </div>)
+      } else {
+        const cards = data.map((obj: Note, index: number) => (
+          <div key={index} className="bg-white p-9 my-9 w-1/2 h-fit border border-black rounded-md">
+            <h3>Titel: {obj.title}</h3>
+            <p>Inehåll: {obj.body}</p>
+          </div>
+        ));
+        setNotes(cards);
+      }
     } catch (error) {
       console.error(error);
     }
