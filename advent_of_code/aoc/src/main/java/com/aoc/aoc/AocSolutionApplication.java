2e535f21ad367728c3eb14b655d2c8fdbfd24497
Björn Tenje Persson <bjorn@tenje.se>
2024-11-22 15:24:10 +0100

    cross origin

--- advent_of_code/aoc/src/main/java/com/aoc/aoc/AocSolutionApplication.java
+++ advent_of_code/aoc/src/main/java/com/aoc/aoc/AocSolutionApplication.java
@@ -2,6 +2,7 @@ package com.aoc.aoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class AocSolutionApplication {
