9efe87dfc882c4ca8e9fe99eb9cc38ba3f0954ec
Björn Tenje Persson <bjorn@tenje.se>
2024-11-29 08:18:41 +0100

    Solution 9 2023

--- advent_of_code/aoc/src/main/java/com/aoc/solutions_2023/S_09.java
+++ advent_of_code/aoc/src/main/java/com/aoc/solutions_2023/S_09.java
@@ -1,5 +1,7 @@
 package com.aoc.solutions_2023;
 
+import java.util.ArrayList;
+
 import com.aoc.lib.*;
 
 public class S_09 extends Solution {
@@ -14,26 +16,68 @@ public class S_09 extends Solution {
 
   public String task_1() {
     String[] lines = input.getLines();
+    Long total = 0l;
     for (String firstLine : lines) {
       String lineNumbers[] = firstLine.split(" ");
-      Long numbers[] = new Long[lineNumbers.length + 1];
-      Long nextMap[][] = new Long[lineNumbers.length][];
+      ArrayList<ArrayList<Long>> lineMapping = new ArrayList<>();
+      lineMapping.add(new ArrayList<>());
       for (int i = 0; i < lineNumbers.length; i++) {
-        numbers[i] = Long.valueOf(lineNumbers[i]);
+        lineMapping.get(0).add(Long.parseLong(lineNumbers[i]));
       }
-      for (int i = 0; i < lineNumbers.length - 1; i++) {
-        for (int j = 0; j < lines.length; j++) {
-
+      Boolean x = true;
+      int index = 0;
+      while (x) {
+        lineMapping.add(new ArrayList<>());
+        for (int i = 0; i < lineMapping.get(index).size() - 1; i++) {
+          long n = lineMapping.get(index).get(i + 1);
+          long n2 = lineMapping.get(index).get(i);
+          lineMapping.getLast().add(n - n2);
         }
+        if (lineMapping.getLast().size() == 1) {
+          x = false;
+        }
+        index++;
       }
+      for (int i = lineMapping.size() - 1; i > 0; i--) {
+        lineMapping.get(i - 1).add(lineMapping.get(i).getLast() + lineMapping.get(i - 1).getLast());
+      }
+      total += lineMapping.getFirst().getLast();
     }
 
-    return "";
+    return Long.toString(total);
   }
 
   public String task_2() {
+    String[] lines = input.getLines();
+    Long total = 0l;
+    for (String firstLine : lines) {
+      String lineNumbers[] = firstLine.split(" ");
+      ArrayList<ArrayList<Long>> lineMapping = new ArrayList<>();
+      lineMapping.add(new ArrayList<>());
+      for (int i = 0; i < lineNumbers.length; i++) {
+        lineMapping.get(0).add(Long.parseLong(lineNumbers[i]));
+      }
+      Boolean x = true;
+      int index = 0;
+      while (x) {
+        lineMapping.add(new ArrayList<>());
+        for (int i = 0; i < lineMapping.get(index).size() - 1; i++) {
+          long n = lineMapping.get(index).get(i + 1);
+          long n2 = lineMapping.get(index).get(i);
+          lineMapping.getLast().add(n - n2);
+        }
+        if (lineMapping.getLast().size() == 1) {
+          x = false;
+        }
+        index++;
+      }
+      for (int i = lineMapping.size() - 1; i > 0; i--) {
+        lineMapping.get(i - 1).add(0, lineMapping.get(i - 1).getFirst() - lineMapping.get(i).getFirst());
+      }
+      total += lineMapping.getFirst().getFirst();
+    }
 
-    return "";
+    return Long.toString(total);
   }
 
 }
