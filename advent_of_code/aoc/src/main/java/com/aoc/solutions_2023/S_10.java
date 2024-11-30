f9c9b4f66adf9055f6597d7bbab704e6585793f3
Björn Tenje Persson <bjorn@tenje.se>
2024-11-30 09:36:34 +0100

    BOILERPLATE

--- advent_of_code/aoc/src/main/java/com/aoc/solutions_2023/S_10.java
+++ advent_of_code/aoc/src/main/java/com/aoc/solutions_2023/S_10.java
@@ -13,7 +13,22 @@ public class S_10 extends Solution {
     InputHandler ip = new InputHandler(input);
     Character[][] m = ip.getMatrix();
     StartingPoint sp = new StartingPoint(m);
+    PointMover pt = new PointMover(sp.getsX(), sp.getsY(), sp.getDirection(), m);
+    pt.nextPoint();
+    while (m[pt.getPosY()][pt.getPosX()] != 'S') {
+      pt.nextPoint();
+    }
+    printMatrix(pt.getNewMap());
     return "";
+  }
+
+  private void printMatrix(Character[][] m) {
+    for (int i = 0; i < m.length; i++) {
+      for (int j = 0; j < m[0].length; j++) {
+        System.out.print(m[i][j]);
+      }
+      System.out.println("");
+    }
   }
 
   /**
@@ -59,6 +74,115 @@ public class S_10 extends Solution {
         }
       }
       direction = "DOWN";
+    }
+
+    public String getDirection() {
+      return direction;
+    }
+
+    public int getsX() {
+      return sX;
+    }
+
+    public int getsY() {
+      return sY;
+    }
+  }
+
+  /**
+   * PointMover
+   */
+  public class PointMover {
+
+    int posX, posY;
+    String direction;
+    Character[][] originalMap, newMap;
+
+    public PointMover(int posX, int posY, String direction, Character[][] originalMap) {
+      this.posX = posX;
+      this.posY = posY;
+      this.direction = direction;
+      this.originalMap = originalMap;
+      newMap = new Character[originalMap.length][originalMap[0].length];
+      for (int i = 0; i < newMap.length; i++) {
+        for (int j = 0; j < newMap[i].length; j++) {
+          newMap[i][j] = '.';
+          System.out.print(newMap[i][j]);
+        }
+        System.out.println("");
+      }
+    }
+
+    public void nextPoint() {
+      newMap[posY][posX] = '+';
+      switch (direction) {
+        case "LEFT":
+          posX--;
+          switch (originalMap[posY][posX]) {
+            case 'L':
+              direction = "UP";
+              break;
+            case 'F':
+              direction = "DOWN";
+              break;
+            default:
+              break;
+          }
+          break;
+        case "RIGHT":
+          posX++;
+          switch (originalMap[posY][posX]) {
+            case 'J':
+              direction = "UP";
+              break;
+            case '7':
+              direction = "DOWN";
+              break;
+            default:
+              break;
+          }
+          break;
+        case "UP":
+          posY--;
+          switch (originalMap[posY][posX]) {
+            case '7':
+              direction = "LEFT";
+              break;
+            case 'F':
+              direction = "RIGHT";
+              break;
+            default:
+              break;
+          }
+          break;
+        case "DOWN":
+          posY++;
+          switch (originalMap[posY][posX]) {
+            case 'L':
+              direction = "RIGHT";
+              break;
+            case 'J':
+              direction = "LEFT";
+              break;
+            default:
+              break;
+          }
+          break;
+        default:
+          break;
+      }
+    }
+
+    public Character[][] getNewMap() {
+      return newMap;
+    }
+
+    public int getPosX() {
+      return posX;
+    }
+
+    public int getPosY() {
+      return posY;
     }
   }
 
