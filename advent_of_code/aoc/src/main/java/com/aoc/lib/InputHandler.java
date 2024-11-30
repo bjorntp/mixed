cb1cef8d297256c34947982825c4293d6e06ebb1
Björn Tenje Persson <bjorn@tenje.se>
2024-11-30 08:25:39 +0100

    Addde matrix creater

--- advent_of_code/aoc/src/main/java/com/aoc/lib/InputHandler.java
+++ advent_of_code/aoc/src/main/java/com/aoc/lib/InputHandler.java
@@ -22,6 +22,21 @@ public class InputHandler {
     return rawInput.split("\n");
   }
 
+  public Character[][] getMatrix() {
+    String[] lines = getLines();
+    Character[][] matrix = new Character[lines.length][];
+    for (int i = 0; i < matrix.length; i++) {
+      matrix[i] = new Character[lines[i].length()];
+      for (int j = 0; j < lines[i].length(); j++) {
+        matrix[i][j] = lines[i].charAt(j);
+        System.out.print(matrix[i][j]);
+      }
+      System.out.println("");
+    }
+
+    return matrix;
+  }
+
   /**
    * @return A string of the original input
    */
